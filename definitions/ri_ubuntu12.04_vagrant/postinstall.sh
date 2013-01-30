5d4563cda077d645239cec4187d9f58aa5b5f372
Ryan J. Geyer <me@ryangeyer.com>
2013-01-29 18:06:28 -0800

    Initial commit of ubuntu 12.04

---
+++ definitions/ri_ubuntu12.04_vagrant/postinstall.sh
@@ -0,0 +1,106 @@
+# postinstall.sh created from Mitchell's official lucid32/64 baseboxes
+
+date > /etc/vagrant_box_build_time
+
+
+# Apt-install various things necessary for Ruby, guest additions,
+# etc., and remove optional things to trim down the machine.
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install linux-headers-$(uname -r) build-essential
+apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
+apt-get -y install vim
+# RightScale RightLink additions
+apt-get -y install libc6 debconf curl git-core
+apt-get clean
+
+# Installing the virtualbox guest additions
+apt-get -y install dkms
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+cd /tmp
+wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
+mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+
+rm VBoxGuestAdditions_$VBOX_VERSION.iso
+rm /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso
+
+# Setup sudo to allow no-password sudo for "sudo"
+usermod -a -G sudo vagrant
+cp /etc/sudoers /etc/sudoers.orig
+sed -i -e 's/%sudo   ALL=(ALL:ALL) ALL/%sudo   ALL=(ALL:ALL) NOPASSWD:ALL/' /etc/sudoers
+
+# Add puppet user and group
+adduser --system --group --home /var/lib/puppet puppet
+
+# Install NFS client
+apt-get -y install nfs-common
+
+# Install Ruby from source in /opt so that users of Vagrant
+# can install their own Rubies using packages or however.
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p290.tar.gz
+tar xvzf ruby-1.9.2-p290.tar.gz
+cd ruby-1.9.2-p290
+./configure --prefix=/opt/ruby
+make
+make install
+cd ..
+rm -rf ruby-1.9.2-p290
+
+# Install RubyGems 1.7.2
+wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.11.tgz
+tar xzf rubygems-1.8.11.tgz
+cd rubygems-1.8.11
+/opt/ruby/bin/ruby setup.rb
+cd ..
+rm -rf rubygems-1.8.11
+
+# Install Chef Omnibus with an embedded ruby.
+# Even though most of that is going to get destroyed by right_image.sh, we need the /opt/chef/bin stubs
+curl -L http://www.opscode.com/chef/install.sh | bash
+
+# Add /opt/ruby/bin to the global path as the last resort so
+# Ruby, RubyGems, and Chef/Puppet are visible
+echo 'PATH=$PATH:/opt/ruby/bin/'> /etc/profile.d/vagrantruby.sh
+
+# Installing vagrant keys
+mkdir /home/vagrant/.ssh
+chmod 700 /home/vagrant/.ssh
+cd /home/vagrant/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
+chmod 600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+
+# RightScale RightLink installation
+mkdir -p /etc/rightscale.d; echo -n none > /etc/rightscale.d/cloud
+cd /tmp
+wget http://mirror.rightscale.com/rightscale_rightlink/5.8.8/ubuntu/rightscale_5.8.8-ubuntu_12.04-amd64.deb
+dpkg -i rightscale_5.8.8-ubuntu_12.04-amd64.deb
+rm rightscale_5.8.8-ubuntu_12.04-amd64.deb
+for i in `find /opt/chef/bin -type f`; do sed -i s,#\!/opt/chef/embedded/bin/ruby,#\!/opt/rightscale/sandbox/bin/ruby,g $i; done
+rm -rf /opt/chef/embedded
+
+# Remove items used for building, since they aren't needed anymore
+apt-get -y remove linux-headers-$(uname -r) build-essential
+apt-get -y autoremove
+
+# Zero out the free space to save space in the final image:
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+
+# Removing leftover leases and persistent rules
+echo "cleaning up dhcp leases"
+rm /var/lib/dhcp3/*
+
+# Make sure Udev doesn't block our network
+# http://6.ptmc.org/?p=164
+echo "cleaning up udev rules"
+rm /etc/udev/rules.d/70-persistent-net.rules
+mkdir /etc/udev/rules.d/70-persistent-net.rules
+rm -rf /dev/.udev/
+rm /lib/udev/rules.d/75-persistent-net-generator.rules
+
+echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
+echo "pre-up sleep 2" >> /etc/network/interfaces
+exit
